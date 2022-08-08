2ff76fb6e69b1738fafc2c10acd7bf1287728029
Rene Schallner <rene@renerocks.ai>
2022-08-08 13:32:19 +0200

    80col fix: src/gitversiontag.zig

--- src/gitversiontag.zig
+++ src/gitversiontag.zig
@@ -1,15 +1,13 @@
 const std = @import("std");
-pub fn gitVersionTag(alloc: std.mem.Allocator) ![]const u8 {
+pub fn gitVersionTag(a: std.mem.Allocator) ![]const u8 {
     const args = [_][]const u8{
         "git",
         "tag",
         "--sort=-creatordate",
     };
 
-    if (std.ChildProcess.exec(.{ .argv = args[0..], .allocator = alloc })) |result| {
-        // print stdout
-        //std.debug.print("{s}\n", .{result.stdout});
-        if (std.mem.split(u8, result.stdout, "\n").next()) |firstline| {
+    if (std.ChildProcess.exec(.{ .argv = args[0..], .allocator = a })) |ret| {
+        if (std.mem.split(u8, ret.stdout, "\n").next()) |firstline| {
             return firstline;
         } else {
             return "unknown";
